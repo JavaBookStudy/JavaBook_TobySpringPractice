f5e2fc6f7c2086b40e5157703035e22a0c54a74d
taxol1203 <taxol1203@pusan.ac.kr>
2021-07-03 15:32:32 +0900

    list 1.30

--- week2/taxol/week2_springP/src/main/java/com/taxol/chapter1_7/CountingDaoFactory.java
+++ week2/taxol/week2_springP/src/main/java/com/taxol/chapter1_7/CountingDaoFactory.java
@@ -3,6 +3,11 @@ package com.taxol.chapter1_7;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/*
+ * list 1.31
+ * CountingConnectionMaker 의존관계가 추가된 DI 설정용 클래스
+ * 이로써 UserDao -> CountingConnectionMaker -> DConnectionMaker의 관계가 성립되었다.
+*/
 @Configuration
 public class CountingDaoFactory {
 	@Bean
