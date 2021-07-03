e246ee490da14f52776768e77fdb52cc0c0598ec
taxol1203 <taxol1203@pusan.ac.kr>
2021-07-03 15:41:52 +0900

    list 1.32

--- week2/taxol/week2_springP/src/main/java/com/taxol/chapter1_7/UserDaoConnectionCountingTest.java
+++ week2/taxol/week2_springP/src/main/java/com/taxol/chapter1_7/UserDaoConnectionCountingTest.java
@@ -9,20 +9,19 @@ import chapter1_7.user.domain.User;
 
 
 /*
- * list 1.19
- * 클라이언트 역할의 UserDaoTest
- * 애플리케이션 컨텍스트를 적용하였다.
+ * list 1.32
+ * CountingConnectionMaker에 대한 테스트 클래스
 */
 public class UserDaoConnectionCountingTest {
 		// 실제 테스트를 위한 main
 		public static void main(String[] args) throws ClassNotFoundException, SQLException{
 			
-			ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+			ApplicationContext context = new AnnotationConfigApplicationContext(CountingDaoFactory.class);
 			
 			UserDao dao = context.getBean("userDao", UserDao.class);
 			
 			User user = new User();
-			user.setId("whitesh11");
+			user.setId("whitesh12");
 			user.setName("baek");
 			user.setPassword("married");
 			
@@ -34,5 +33,9 @@ public class UserDaoConnectionCountingTest {
 			System.out.println(user2.getName());
 			System.out.println(user2.getPassword());
 			System.out.println(user2.getId() + " 조회 성공");
+			
+			
+			CountingConnectionMaker ccm = context.getBean("connectionMaker", CountingConnectionMaker.class);
+			System.out.println("Connection counter : " + ccm.getCounter());
 		}
 }
